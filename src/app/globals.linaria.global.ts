6edf63a12a270f1fa106149b73bc42cb757f5eb7
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-09-15 19:29:09 +0400

    Prevent double-scrolling on mobiles #2

--- src/app/globals.linaria.global.ts
+++ src/app/globals.linaria.global.ts
@@ -1,14 +1,18 @@
 import { css } from '@linaria/core';
+import { media } from './utils/media-queries';
 
 export const globals = css`
   :global() {
-    html {
-      min-height: 100vh;
-    }
     body {
       background: #1e1e1e;
       margin: 0;
       padding: 0;
+    }
+    html,
+    body {
+      ${media.mobileStyle} {
+        overscroll-behavior-y: none;
+      }
     }
   }
 `;
