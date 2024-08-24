e93028190c426bcc058e537115acb2d928a869e1
Art Ginzburg <ginzburg.arthur@gmail.com>
2024-08-24 21:52:41 +0400

    feat: Make sitemap static

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,25 +1,22 @@
-import { headers } from 'next/headers';
-
 import { navigationSections } from './config/navigationSections';
 
 import type { MetadataRoute } from 'next';
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  const headersList = headers();
-  const host = headersList.get('host') ?? 'localhost:3000';
-  const proto = headersList.get('x-forwarded-proto') ?? 'https';
-  const origin = `${proto}://${host}`;
+const { CANONICAL_HOST } = process.env;
+const origin = CANONICAL_HOST ? `https://${CANONICAL_HOST}` : `http://localhost:3000`;
 
+export default function sitemap(): MetadataRoute.Sitemap {
   const currentDate = new Date();
+
+  const sharedOptions = {
+    lastModified: currentDate,
+    changeFrequency: 'monthly',
+  } satisfies Omit<MetadataRoute.Sitemap[number], 'url'>;
 
   const navigationSectionsSitemap: MetadataRoute.Sitemap = navigationSections.map((section) => ({
     url: `${origin}/#${section}`,
-    lastModified: currentDate,
-    changeFrequency: 'monthly',
+    ...sharedOptions,
   }));
 
-  return [
-    { url: origin, lastModified: currentDate, changeFrequency: 'monthly' },
-    ...navigationSectionsSitemap,
-  ];
+  return [{ url: origin, ...sharedOptions }, ...navigationSectionsSitemap];
 }
