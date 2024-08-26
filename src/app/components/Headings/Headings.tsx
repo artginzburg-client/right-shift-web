1bb8556da46b0f7aa49d129ab1d95ba058fe0a13
Art Ginzburg <ginzburg.arthur@gmail.com>
2024-08-26 10:00:30 +0400

    experiment: Wake up, Neo

--- src/app/components/Headings/Headings.tsx
+++ src/app/components/Headings/Headings.tsx
@@ -29,10 +29,37 @@ export function Headings({
     setIsPressingRightShift(false);
   });
 
+  const [isFocused, setIsFocused] = useState(true);
+  const [blurredCount, setBlurredCount] = useState(0);
+  const blurredCountTextMap: Record<number, string | undefined> = {
+    1: 'Wake up, Neo...',
+    2: 'The Matrix has you...',
+    3: 'Follow the white rabbit.',
+    4: 'Knock, knock, Neo.',
+  };
+  const blurredText = blurredCountTextMap[blurredCount];
+  useEventListener(
+    'blur',
+    () => {
+      setIsFocused(false);
+      setBlurredCount((prev) => prev + 1);
+    },
+    undefined,
+    true,
+  );
+  useEventListener(
+    'focus',
+    () => {
+      setIsFocused(true);
+    },
+    undefined,
+    true,
+  );
+
   return (
     <HeadingsContainer data-is-menu-opened={isMenuOpened} data-has-opened-section={!!openedSection}>
       <Heading1 style={{ transform: isPressingRightShift ? 'scale(0.7)' : undefined }}>
-        right.shift
+        {isFocused ? 'right.shift' : blurredText}
       </Heading1>
       <Subheading>
         digital products <RotatingWordInSubheading /> <SubheadingSpan>for you</SubheadingSpan>
