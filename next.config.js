07452f4709936146c5da1263d8664c0b94632875
artginzburg <millet.path-27@icloud.com>
2026-07-25 08:01:04 +0300

    chore: static export for GitHub Pages; move server routes to private folders

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const withLinaria = require('next-with-linaria')
 
 /** @type {import('next-with-linaria').LinariaConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/right-shift-web',
+  images: { unoptimized: true },
   experimental: {
     webpackBuildWorker: true,
     typedRoutes: true
